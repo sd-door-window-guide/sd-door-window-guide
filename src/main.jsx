274ca7ec0898deb18e6e88a74f7a3ab494a27da5
sd-door-window-guide <john.c.johnsen@gmail.com>
2026-08-08 10:17:52 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Blog from "./Blog.jsx";
+import BlogPost from "./BlogPost.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
